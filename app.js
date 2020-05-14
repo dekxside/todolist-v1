154f8d47e28b7d992713224f6b5e6f1e0bb94499
Douglas Tiepolo <dvtiepolo@gmail.com>
2020-05-14 17:47:38 +0100

    update production

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -13,25 +15,112 @@ app.use(express.static("public"));
 const items = [];
 const workItems = [];
 
+mongoose.set('useFindAndModify', false);
+mongoose.connect("mongodb+srv://dodo-admin:admin-dodo@cluster0-w8out.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+    name: "Welcome to your To Do list!"
+});
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+    name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/", (req, res) => {
-    const day = date.getDate();
-    res.render("list", {listTitle: day, newListItems: items});
+    // const day = date.getDate();
+    // res.render("list", {listTitle: day, newListItems: items});
+
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully saved default items to DB.");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", {listTitle: "Today", newListItems: foundItems});
+        }
+    });
 });
 
 app.post("/", (req, res) => {
-    const item = req.body.newItem;
-    
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+
+    const item = new Item({
+        name: itemName
+    });
+
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");    
     } else {
-        items.push(item);
-        res.redirect("/");
+        List.findOne({name: listName}, (err, foundList) => {
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
     }
+
+    
 });
 
-app.get("/work", (req, res) => {
-    res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/:customListName", (req, res) => {
+    const customListName = _.capitalize(req.params.customListName);
+    
+    List.findOne({name: customListName}, (err, foundList) => {
+        if (!err) {
+            if (!foundList) {
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/" + customListName);
+            } else {
+                res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+            }
+        }
+    });
+});
+
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+    
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId, function(err) {
+            if (!err) {
+                console.log("Successfully deleted checked item");
+                res.redirect("/");
+            }
+        });    
+    } else {
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, (err, foundList) => {
+            if (!err) {
+                res.redirect("/" + listName);
+            }
+        });
+    }
 });
 
 app.listen(3000, () => console.log("Server is running os port 3000"));
